d83f46ce1dc1713ed079ec4200cf55faf12ff329
Shivam Bhardwaj <sb6377@nyu.edu>
2022-05-21 06:46:34 -0700

    Update solution.cpp

--- solutions/problem_1/solution.cpp
+++ solutions/problem_1/solution.cpp
@@ -1,1 +1,19 @@
-// put the solution here
+#include<bits/stdc++.h>
+
+class Solution {
+public:
+    int count_digits(int n){
+        string s = to_string(n);
+        return s.size();
+    }
+    
+    int findNumbers(vector<int>& nums) {
+        int count = 0;
+        for (auto i : nums){
+            if(count_digits(i)%2){
+                count++;
+            }
+        }
+        return nums.size() - count;
+    }
+};
