1099ceae4734620be2b1405d0051ebde50a8df8f
Shivam Bhardwaj <sb6377@nyu.edu>
2022-05-23 22:40:02 -0700

    Create solution.cpp

---
+++ solutions/problem_3/solution.cpp
@@ -0,0 +1,37 @@
+// Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right.
+
+// Note that elements beyond the length of the original array are not written. Do the above modifications to the input array in place and do not return anything.
+
+ 
+
+// Example 1:
+
+// Input: arr = [1,0,2,3,0,4,5,0]
+// Output: [1,0,0,2,3,0,0,4]
+// Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+// Example 2:
+
+// Input: arr = [1,2,3]
+// Output: [1,2,3]
+// Explanation: After calling your function, the input array is modified to: [1,2,3]
+ 
+
+// Constraints:
+
+// 1 <= arr.length <= 104
+// 0 <= arr[i] <= 9
+
+// SOlution
+
+
+vector<int> ans;
+        for(int &i : arr)
+        {
+            if(i == 0)
+                ans.push_back(0);
+            ans.push_back(i);
+        }
+        for(int i=0; i<arr.size();i++)
+        {
+            arr[i] = ans[i];
+        }
