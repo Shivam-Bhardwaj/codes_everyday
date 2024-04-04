cbd02de443780225c1770d309aa7e2b298092cc0
shivam-bhardwaj <sb6377@nyu.edu>
2024-04-03 21:06:55 -0700

    solution

--- solutions/1768. Merge Strings Alternately.py
+++ solutions/1768. Merge Strings Alternately.py
@@ -1,2 +1,4 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
+        n = min(len(word1), len(word2))
+        return ''.join([word1[i] + word2[i] for i in range(n)]) + word1[n:] + word2[n:]
